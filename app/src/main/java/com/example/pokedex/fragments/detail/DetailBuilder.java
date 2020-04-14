2073890e483aa5cff2f0203cf13e40e822a5490e
kkgosu <kostya96.08@mail.ru>
2020-04-14 11:28:35 +0300

    commit1

--- app/src/main/java/com/example/pokedex/fragments/detail/DetailBuilder.java
+++ app/src/main/java/com/example/pokedex/fragments/detail/DetailBuilder.java
@@ -12,7 +12,6 @@ public class DetailBuilder extends BaseBuilder<DetailFragment> {
 
     public DetailBuilder() {
     }
-
     public DetailBuilder(int pokemonId) {
         this.pokemonId = pokemonId;
     }
